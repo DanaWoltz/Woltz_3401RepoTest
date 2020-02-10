c89a0f05ef0b42791708a940d1488fd84bc1fa61
DanaWoltz <50628247+DanaWoltz@users.noreply.github.com>
2020-02-10 16:41:05 -0500

    week3
    homework

---
+++ 3401_Week3_Woltz/Assets/MoveObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(1, 0, 0));
+    }
+}
